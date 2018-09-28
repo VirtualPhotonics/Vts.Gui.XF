c65fe46d17fa550d0b06857462c9ad1df1a72ef5
hayakawa <hayakawa@uci.edu>
2018-09-28 16:03:27 -0700

    Kind of have WavelengthRange working.  Formatting needs work.  Rest of app needs tweaking too.

---
+++ Vts.Gui.XF/Vts.Gui.XF/View/Controls/RangeView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Vts.Gui.XF.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vts.Gui.XF.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RangeView : ContentView
+    {
+        private RangeViewModel viewModel;
+        public RangeView()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new RangeViewModel();
+        }
+
+        private void TextBox_KeyDown(object sender)
+        {
+        //    var tbx = sender as TextBox;
+        //    if (tbx != null && e.Key == Key.Enter)
+        //        tbx.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+    }
+}
