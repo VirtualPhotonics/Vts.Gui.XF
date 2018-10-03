68ee0fafbfb9b060225cf2383bc03492c021e86a
Lisa Malenfant <lisa.malenfant@uci.edu>
2018-10-03 09:46:23 -0700

    Added a settings panel and included the font awesome fonts to use in the application

---
+++ Vts.Gui.XF/Vts.Gui.XF/View/Panels/SubPanels/PlotViewSettings.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Vts.Gui.XF.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Vts.Gui.XF.View.Panels.SubPanels
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PlotViewSettings : ContentPage
+	{
+	    private PlotViewModel _viewModel;
+		public PlotViewSettings ()
+		{
+			InitializeComponent ();
+		    BindingContext = _viewModel = PanelsListViewModel.Current.PlotVM;
+		}
+
+	    async void OnCloseButton_Clicked(object sender, EventArgs args)
+	    {
+	        await Navigation.PopModalAsync();
+	    }
+    }
+}
